7b4ead9d2498013a260d4cf66c5c4b9fa42c3dec
rajca <rajca@RAJCANNA>
2017-02-09 19:10:56 -0600

    Add project files.

---
+++ Binary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
